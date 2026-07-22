47b3086566b3cc9c541047a3a4efc7483162eb80
jayvient0 <enjoelijah.ee@gmail.com>
2026-07-22 14:33:14 +0100

    feat:added active nd V2

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.json({ message: "Hello from DevOps Mini Pipeline!", status: "Healthy and Active" });
+  res.json({ message: "Hello from DevOps Mini Pipeline!V2", status: "Healthy and Active" });
 });
 
 app.listen(PORT, () => {
